b549f1f1bb540c49567b21cc2822aff634139e37
victor diaz <vdmpeniel@yahoo.es>
2023-08-22 17:44:31 -0400

    Finished the first task "Strategy Design Pattern" (No way to know if it is correct for now)

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,6 +1,10 @@
 package com.mjc.stage2;
 
+import com.mjc.stage2.impl.ManufactureFilteringStrategy;
+import com.mjc.stage2.impl.MaxPriceFilteringStrategy;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopStock {
     private final List<Product> productList;
@@ -9,5 +13,40 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    // Write your code here!
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+        return productList.stream()
+            .filter(product -> filteringStrategy.filter(product))
+            .collect(Collectors.toList());
+    }
+
+
+    public static void main(String[] args) {
+
+        List<Product> productList = List.of(
+                Product.builder()
+                        .manufacture("Apple")
+                        .name("iphone")
+                        .price(120)
+                        .build(),
+                Product.builder()
+                        .manufacture("Apple")
+                        .name("Mac Book")
+                        .price(1800)
+                        .build(),
+                Product.builder()
+                        .manufacture("Dell")
+                        .name("alienware")
+                        .price(1800)
+                        .build(),
+                Product.builder()
+                        .manufacture("LENOVO")
+                        .name("thinkpad")
+                        .price(500)
+                        .build()
+
+        );
+        ShopStock shopStock = new ShopStock(productList);
+        System.out.println(shopStock.executeFilteringStrategy(new ManufactureFilteringStrategy("Apple")));
+        System.out.println(shopStock.executeFilteringStrategy(new MaxPriceFilteringStrategy(1800)));
+    }
 }
--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
@@ -1,11 +1,26 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.FilteringStrategy;
+import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
-public class MaxPriceFilteringStrategy {
+public class MaxPriceFilteringStrategy implements FilteringStrategy {
     private double maxPrice;
 
-    // Write your code here!
+
+    private double calculateMaxPrice(List<Product> productList){
+        return productList.stream()
+                .map(product -> product.getPrice())
+                .max(Double::compare)
+                .orElse(null);
+    }
+
+    @Override
+    public boolean filter(Product product) {
+        return product.getPrice() == maxPrice;
+    }
 }
