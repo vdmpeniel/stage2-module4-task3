82c87cc36a352bc8b3ee43be82ba7bc3eb3db2b1
victor diaz <vdmpeniel@yahoo.es>
2023-08-24 02:30:05 -0400

    Corrected regex names.

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,15 +1,12 @@
 package com.mjc.stage2.parser;
 
 import com.mjc.stage2.entity.AbstractTextComponent;
-import com.mjc.stage2.entity.SymbolLeaf;
 import com.mjc.stage2.entity.TextComponent;
 import com.mjc.stage2.entity.TextComponentType;
 import com.mjc.stage2.tokenizer.impl.TokenizerImpl;
 
-import java.util.List;
-
 public class LexemeParser extends AbstractTextParser{
-    private static final String WORD_REGEX = "([\\w!=?()\\.':]+)";
+    private static final String LEXEME_REGEX = "([\\w!=?()\\.':]+)";
 
     public LexemeParser(){}
     public LexemeParser(AbstractTextParser nextParser){
@@ -18,7 +15,7 @@ public class LexemeParser extends AbstractTextParser{
 
     @Override
     public void parse(AbstractTextComponent abstractTextComponent, String string) {
-        TokenizerImpl.getMatches(WORD_REGEX, string)
+        TokenizerImpl.getMatches(LEXEME_REGEX, string)
             .stream()
             .forEach(part -> {
                 AbstractTextComponent inner = new TextComponent(TextComponentType.SYMBOL);
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -3,21 +3,16 @@ package com.mjc.stage2.parser;
 
 import com.mjc.stage2.entity.AbstractTextComponent;
 import com.mjc.stage2.entity.SymbolLeaf;
-import com.mjc.stage2.entity.TextComponent;
-import com.mjc.stage2.entity.TextComponentType;
 import com.mjc.stage2.tokenizer.impl.TokenizerImpl;
-
-import java.util.List;
-import java.util.Objects;
 
 public class WordParser extends AbstractTextParser{
 
-    private static final String LEXEME_REGEX = "\\S"; //""[\\w!=?()\\.':]";
+    private static final String WORD_REGEX = "\\S"; //""[\\w!=?()\\.':]";
 
 
     @Override
     public void parse(AbstractTextComponent abstractTextComponent, String string) {
-        TokenizerImpl.getMatches(LEXEME_REGEX, string)
+        TokenizerImpl.getMatches(WORD_REGEX, string)
             .forEach(part -> {
                 abstractTextComponent.add(new SymbolLeaf(null, part.charAt(0)));
             });
