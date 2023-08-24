e373ec151eaf8570c3779433470bb07daec5640c
victor diaz <vdmpeniel@yahoo.es>
2023-08-24 00:54:26 -0400

    Recovered the Tokenizer interface

---
+++ task3-handling/src/main/java/com/mjc/stage2/tokenizer/Tokenizer.java
@@ -0,0 +1,13 @@
+package com.mjc.stage2.tokenizer;
+
+import java.util.List;
+
+public interface Tokenizer {
+    static List<String> getMatches(String regex, String string){
+        return null;
+    }
+    static List<String> simpleSplitter(String regex, String string){
+        return null;
+    }
+    static void print(List<String> results){}
+}
